cba2850b77f77380b6afb2873a542695c19f7a71
Nathan Keeys <nathankeeys@nathans-mbp.myfiosgateway.com>
2023-08-14 22:23:39 -0400

    update base url

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
 import React from "react";
-// import Navigation from "../components/Navigation/Navigation";
-// import PromoBanner from "../components/PromoBanner/PromoBanner";
-// import Footer from "../components/Footer/Footer";
+import Navigation from "components/Navigation/Navigation";
+import PromoBanner from "components/PromoBanner/PromoBanner";
+import Footer from "components/Footer/Footer";
 
 const Layout = ({ children }) => {
 	return (
 		<div className="layout">
-			{/* <Navigation />
-			<PromoBanner /> */}
+			<Navigation />
+			<PromoBanner />
 			{children}
-			{/* <Footer /> */}
+			<Footer />
 		</div>
 	);
 };
