e923c2e7d419853d768e7faf5f94da414be88b76
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2023-09-17 22:44:07 -0400

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navigation from "../components/Navigation/Navigation";
 import PromoBanner from "../components/PromoBanner/PromoBanner";
 import Footer from "../components/Footer/Footer";
 
 const Layout = ({ children }) => {
 	return (
 		<div className="layout">
+			<Navigation />
 			<PromoBanner />
 			{children}
 			<Footer />
